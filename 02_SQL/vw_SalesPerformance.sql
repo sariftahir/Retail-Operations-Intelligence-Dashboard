d4124b4bf51b76cbc0a028cfcc6f9f59d5706cb1
Sarif Tahir <sarif2744@gmail.com>
2026-07-22 15:56:15 +0800

    Update vw_SalesPerformance.sql

--- 02_SQL/vw_SalesPerformance.sql
+++ 02_SQL/vw_SalesPerformance.sql
@@ -1,1 +1,30 @@
+CREATE VIEW vw_SalesPerformance AS
+SELECT 
+    s.OrderID,
+    s.OrderDate,
+    s.ProductID,
+    p.ProductName,
+    p.Category,
+    p.SubCategory,
+    p.Brand,
+    p.DemandTier,
+    s.Quantity AS QtySold,
+    p.UnitCost,
+    p.SellingPrice,
+    s.Revenue,
+    
+    (s.Quantity * p.UnitCost) AS TotalCost,
+    (s.Revenue - (s.Quantity * p.UnitCost)) AS GrossProfit,
+    ISNULL(w.Location, 'Unknown') AS WarehouseLocation,
+    ISNULL(w.Region, 'Unknown') AS WarehouseRegion
 
+FROM dbo.sales_outflow s
+INNER JOIN dbo.products p 
+    ON s.ProductID = p.ProductID
+LEFT JOIN (
+    SELECT DISTINCT ProductID, WarehouseID 
+    FROM dbo.inventory
+) inv 
+    ON s.ProductID = inv.ProductID
+LEFT JOIN dbo.warehouses w 
+    ON inv.WarehouseID = w.WarehouseID;
